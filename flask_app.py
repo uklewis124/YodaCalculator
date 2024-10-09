ada956cb5192618c91ed3c51a01f72c0e5f3454f
lewis davies <lewisastondavies1@gmail.com>
2024-10-09 14:44:30 +0100

    Refactor API data retrieval in index function

--- flask_app.py
+++ flask_app.py
@@ -20,24 +20,33 @@ def page_not_found(e):
 
 @app.route('/')
 def index():
-    people = {}
+    people = []
     current_page = 0
+    all_people_gathered = False
     
-    api_url = PERSON_API.format("?page={}".format(current_page))
-    
-    response = req.get(api_url)
-    if response.status_code == 200:
-        response = response.json()
-        returned_people = response["results"]
+    while not all_people_gathered:
+        current_page += 1
+        print("iteration ran")    
+        api_url = PERSON_API.format("?page={}".format(current_page))
         
-        for person in returned_people:
-            print(person["name"])
-        
-        if DEBUGGING:
-            print("[WARN]", Fore.YELLOW, "Saving JSON Request's Response to output.json") 
-            with open("output.json", 'w') as f:
-                js.dump(response, f, indent=4)
-                print("[WARN]", Fore.GREEN, "Successfully Saved JSON Request")
+        response = req.get(api_url)
+        if response.status_code == 200:
+            response = response.json()
+            returned_people = response["results"]
+            
+            for person in returned_people:
+                people.append(person)
+                print(person["name"])
+            
+            if DEBUGGING:
+                print("[WARN]", Fore.YELLOW, "Saving JSON Request's Response to output.json") 
+                with open("output.json", 'w') as f:
+                    js.dump(response, f, indent=4)
+                    print("[WARN]", Fore.GREEN, "Successfully Saved JSON Request")
+        elif response.status_code == 404:
+            print(response.content)
+            all_people_gathered = True
+    print(people)
     return "Homepage", 200
 
 if __name__ == "__main__":
